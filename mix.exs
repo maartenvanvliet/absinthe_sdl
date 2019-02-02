6aa641b86f03a14a85efb1c3bf39c01724ce0360
Maarten van Vliet <maartenvanvliet@gmail.com>
2019-02-02 11:12:51 +0100

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AbsintheSdl.MixProject do
   def project do
     [
       app: :absinthe_sdl,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
